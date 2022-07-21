a5f69b4c187b55d19b8f94f498872f4c59f16e54
Ioannis Vardas <vardas@par.tuwien.ac.at>
2022-07-21 11:19:46 +0200

    Try on hydra

--- ref/main.c
+++ ref/main.c
@@ -35,7 +35,69 @@
 #include "comm.h"
 #include "timer.h"
 #include "proto.h"
-#include "monitor.h"
+
+
+struct periodic_info {
+	int timer_fd;
+	unsigned long long wakeups_missed;
+};
+
+
+static int make_periodic(unsigned int period, struct periodic_info *info)
+{
+	int ret;
+	unsigned int ns;
+	unsigned int sec;
+	int fd;
+	struct itimerspec itval;
+
+	/* Create the timer */
+	fd = timerfd_create(CLOCK_MONOTONIC, 0);
+	info->wakeups_missed = 0;
+	info->timer_fd = fd;
+	if (fd == -1)
+		return fd;
+
+	/* Make the timer periodic */
+	sec = period / 1000000;
+	ns = (period - (sec * 1000000)) * 1000;
+	itval.it_interval.tv_sec = sec;
+	itval.it_interval.tv_nsec = ns;
+	itval.it_value.tv_sec = sec;
+	itval.it_value.tv_nsec = ns;
+	ret = timerfd_settime(fd, 0, &itval, NULL);
+	return ret;
+}
+
+static void wait_period(struct periodic_info *info)
+{
+	unsigned long long missed;
+	int ret;
+
+	/* Wait for the next timer event. If we have missed any the
+	   number is written to "missed" */
+	ret = read(info->timer_fd, &missed, sizeof(missed));
+	if (ret == -1) {
+		perror("read timer");
+		return;
+	}
+
+	info->wakeups_missed += missed;
+}
+
+
+static void *monitor(void *arg)
+{
+	struct periodic_info info;
+
+	printf("Thread 1 period 200ms\n");
+	make_periodic(200000, &info);
+	while (1) {
+		wait_period(&info);
+        fprintf(stderr,"%llu\n",its);
+	}
+	return NULL;
+}
 
 int main(int argc, char** argv)
 {
--- ref/monitor.h
+++ ref/monitor.h
@@ -1,14 +1,6 @@
 #ifndef MONITOR_H_
 #define MONITOR_H_
 
-struct periodic_info {
-	int timer_fd;
-	unsigned long long wakeups_missed;
-};
-
-static unsigned long long its = 0;
-static unsigned long long accumulator = 0;
-static double avg_times_p = 0.0;
 
 static void *monitor(void *arg);
 static void wait_period(struct periodic_info *info);
